0b1702a76f7f116790cc2e986e3059e415f74018
Crazy5202 <maximkuts23@gmail.com>
2025-01-09 17:33:18 +0300

    Created pdf merger, yay

---
+++ utility/merge_pdf.py
@@ -0,0 +1,21 @@
+from pypdf import PdfWriter
+import glob
+import os
+
+path = "G:\Мои документы\Отсканированные документы\\"
+num = 5
+
+pdfs = []
+
+all_pdfs = sorted(glob.glob(path+"*.pdf"), key=os.path.getmtime)
+
+for file in all_pdfs[-num:]:
+    pdfs.append(file)
+
+merger = PdfWriter()
+
+for pdf in pdfs:
+    merger.append(pdf)
+
+merger.write(path+"merged.pdf")
+merger.close()
