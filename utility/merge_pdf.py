536c41f101c94133a48402ac678e22233b98670d
Crazy5202 <maximkuts23@gmail.com>
2025-11-10 18:53:52 +0300

    Upgraded pdf merger to also work with images

--- utility/merge_pdf.py
+++ utility/merge_pdf.py
@@ -1,21 +1,35 @@
 from pypdf import PdfWriter
+from PIL import Image
 import glob
 import os
 
-path = "G:/Мои документы/Отсканированные документы/Обновлённое/М/"
-num = 4
+path = "C:/Users/Maxim/Downloads/Telegram Desktop/"
+num = 2
+image = True
 
-pdfs = []
+if image:
+    all_imgs = sorted(glob.glob(path+"*.jpg"), key=os.path.getmtime)
+
+    counter = 0
+
+    for image_file in all_imgs[-num:]:
+        img = Image.open(image_file)
+
+        if img.mode != 'RGB':
+            img = img.convert('RGB')
+
+        temp_pdf_path = path+ f"temp_{counter}.pdf"
+
+        img.save(temp_pdf_path, "PDF", resolution=100.0)
+
+        counter += 1
 
 all_pdfs = sorted(glob.glob(path+"*.pdf"), key=os.path.getmtime)
 
-for file in all_pdfs[-num:]:
-    pdfs.append(file)
-
 merger = PdfWriter()
 
-for pdf in pdfs:
-    merger.append(pdf)
+for file in all_pdfs[-num:]:
+    merger.append(file)
 
 merger.write(path+"merged.pdf")
 merger.close()
